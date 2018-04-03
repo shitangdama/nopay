1359d55500676e8e037216ae8c304b9240876d57
shitangdama <kbr1990117@gmail.com>
2018-04-03 13:43:05 +0800

    feature_first_commit

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ### 重构量化项目
 - 收集策略和模型
 - 重写多因子模型
-- 重写dataview,重写ic因子部分，导入ta_lib
+- 从quantos里面转移到ricequant上
---
+++ src/test_demo/config/config.py
@@ -0,0 +1,11 @@
+token = "eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVfdGltZSI6IjE1MjI0MjUxMzgwNjIiLCJpc3MiOiJhdXRoMCIsImlkIjoiMTM1NjQ1NDIxNDUifQ.eDmGunsBAch6Q4IUJJIFKl6z01esSijBHkv6U1EZHoA"
+data_config = {
+    "remote.data.address": "tcp://data.tushare.org:8910",
+    "remote.data.username": "13564542145",
+    "remote.data.password": token
+}
+trade_config = {
+    "remote.trade.address": "tcp://gw.quantos.org:8901",
+    "remote.trade.username": "13564542145",
+    "remote.trade.password": token
+}
